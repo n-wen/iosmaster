0004a619453a9d4d6c60ab52de0c8198e9b894bd
wenning <ht.wenning@foxmail.com>
2019-12-03 01:12:27 +0800

    build and run test complete

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const cp = require('child_process')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,19 +13,45 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "iosmaster" is now active!');
-
+	console.log('"iosmaster" is now active!');
+	const projectRoot = vscode.workspace.workspaceFolders[0].uri.path;
+	console.log("project root: " + projectRoot);
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let buildDisposable = vscode.commands.registerCommand('extension.build', function () {
 		// The code you place here will be executed every time your command is executed
-
+		cp.exec('xcodebuild build -workspace Foo.xcworkspace -scheme Foo -allowProvisioningUpdates  -derivedDataPath build -destination \'platform=iOS Simulator,name=iPhone 6,OS=12.1\'', 
+		{cwd: projectRoot},
+		(err, stdout, stderr) => {
+				console.log('stdout: ' + stdout);
+				console.log('stderr: ' + stderr);
+				if (err) {
+						console.log('error: ' + err);
+				}
+		});
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('build success');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(buildDisposable);
+	let runDisposable = vscode.commands.registerCommand('extension.run', function () {
+		// The code you place here will be executed every time your command is executed
+		
+		cp.exec('ios-sim launch build/Build/Products/Debug-iphonesimulator/Foo.app',
+		{cwd: projectRoot},
+		(err, stdout, stderr) => {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (err) {
+					console.log('error: ' + err);
+			}
+	});
+		// Display a message box to the user
+		vscode.window.showInformationMessage('running');
+	});
+
+	context.subscriptions.push(runDisposable);
 }
 exports.activate = activate;
 
